80f7c31ffd035b0c24edf1f90a97c591406e5c34
Ольга Цыб <dev01@varius.local>
2020-02-25 11:34:45 +0200

    fixed

--- src/Components/Dashboard/Employees/UserProfile/UserProfile.js
+++ src/Components/Dashboard/Employees/UserProfile/UserProfile.js
@@ -37,12 +37,31 @@ export default class UserProfile extends Component{
     };
 
 
-    changeHandler (value, controlName) {
+    // changeHandler (value, controlName) {
+    //     const formControls = {...this.state.formControls};
+    //     const control = {...formControls[controlName]};
+    //
+    //     //control.touched = true;
+    //     control.value = value;
+    //     control.valid = validate(control.value, control.validation);
+    //     formControls[controlName] = control;
+    //     this.setState({
+    //         formControls,
+    //         isFormValid: validateForm(formControls)
+    //     })
+    // }
+    changeHandler = (value, controlName, index) =>{
         const formControls = {...this.state.formControls};
         const control = {...formControls[controlName]};
 
-        //control.touched = true;
-        control.value = value;
+        control.touched = true;
+        const partOne = control.value.slice(0, index)
+        const partTwo = control.value.slice(index + 1)
+        control.value = [
+            ...partOne,
+            value,
+            ...partTwo
+        ];
         control.valid = validate(control.value, control.validation);
 
         formControls[controlName] = control;
@@ -54,6 +73,7 @@ export default class UserProfile extends Component{
     inputControls() {
         return Object.keys(this.state.formControls).map((controlName, index) => {
             const control = this.state.formControls[controlName];
+            console.log(control.value);
             return (
                 <Input
                     value={control.value[index]}
@@ -61,7 +81,7 @@ export default class UserProfile extends Component{
                     type="text"
                     key={controlName + index}
                     //touched={control.touched}
-                    onChange={event => this.changeHandler(event.target.value, controlName)}
+                    onChange={event => this.changeHandler(event.target.value, controlName, index)}
                 />
             )
         })
